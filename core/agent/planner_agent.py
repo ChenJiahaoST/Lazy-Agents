094a81f2a32e4dd68c9825e1019e066f0a8b3bb8
Chenjiahao <chenjiahao@sensetime.com>
2025-03-12 14:54:49 +0800

    deep research using lazyllm

---
+++ core/agent/planner_agent.py
@@ -0,0 +1,21 @@
+from lazyllm import OnlineChatModule
+
+from core.prompt.planner_prompt import TOC_PLAN_INSTRUCTION
+from core.tools.web_tools import web_search, visit_url
+from core.tools.plan_tools import get_more_info_from_user
+from core.agent.agent import CustomReactAgent
+
+
+def create_plan_agent() -> CustomReactAgent:
+    agent = CustomReactAgent(
+        llm=OnlineChatModule(source="qwen", model="qwen-plus", stream=False),
+        tools=["get_more_info_from_user", "web_search", "visit_url"],
+        custom_prompt=TOC_PLAN_INSTRUCTION,
+        max_retries=10,
+        return_trace=True
+    )
+    return agent
+
+def test():
+    plan_agent = create_plan_agent()
+    print(plan_agent("1"))
---
+++ core/tools/__init__.py
@@ -0,0 +1,2 @@
+from core.tools.plan_tools import get_more_info_from_user
+from core.tools.web_tools import web_search, visit_url
